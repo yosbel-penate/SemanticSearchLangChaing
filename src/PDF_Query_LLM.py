b720fa7e935b3e4c2cbf28019cadad4f465ba24c
Corso <58090876+yosbel-penate@users.noreply.github.com>
2023-04-16 20:15:58 -0400

    first commit

---
+++ src/PDF_Query_LLM.py
@@ -0,0 +1,4 @@
+from PyPDF2 import PdfReader
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import ElasticVectorSearch, Pinecone, Weaviate, FAISS
