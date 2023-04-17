29e9ad9bc66e6c3b01262d6c9a3190e63bdb7d14
Corso <58090876+yosbel-penate@users.noreply.github.com>
2023-04-17 18:38:45 -0400

    upload and view pdf files

--- app.py
+++ app.py
@@ -1,8 +1,49 @@
-from flask import Flask
+import os
 from flask import render_template
+from flask import Flask, flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
+from flask import send_from_directory
+
+UPLOAD_FOLDER = 'store/pdf'
+ALLOWED_EXTENSIONS = {'pdf'}
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route("/")
-def hello_world():
-    return render_template('index.html')
+def allowed_file(filename):
+    return '.' in filename and \
+            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('download_file', name=filename))
+    return render_template('load_form.html')
+
+@app.route('/files/<name>')
+def download_file(name):
+    return send_from_directory(app.config["UPLOAD_FOLDER"], name)
+
+@app.route('/files')
+def dir_listing():
+    BASE_DIR = 'store/pdf'
+
+    # Joining the base and the requested path
+    abs_path = os.path.join(BASE_DIR, '')
+
+    # Show directory contents
+    files = os.listdir(abs_path)
+    return render_template('files.html', files=files)
